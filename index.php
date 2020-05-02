a6ed584b097bcff150bb4dc994978d3a19c66ef0
Simon <bertilzon.simon@gmail.com>
2020-05-02 02:20:28 +0200

    populated files

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0">
+		<title> Platitude </title>
+        <link rel="stylesheet" type="text/css" href="stylesheet.css">
+	</head>
+
+    <script>
+
+        function store(){
+            //document.getElementById("infocontent").innerHTML = "Hello";
+            window.open("store.php", "_parent","",true);
+        }
+
+    </script>
+
+    <body>
+        <div class="noselect">
+            <div class="headline"> Platitude </div>
+            <button class="continuebutton" onclick="store();">browse store</button>
+        </div>
+        
+        <p id="infocontent"> </p>
+
+        <div class="infocontainer">
+            <div class="infoChild">
+                <span> Lorem </span>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor quae quis incidunt, officia animi, fugiat aspernatur magnam eos veritatis eius reprehenderit blanditiis, labore quo ipsum sit praesentium cupiditate quaerat culpa!</p>
+            </div>
+
+            <div class="infoChild">
+                <span> Lorem </span>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime non sequi cum voluptates ad maiores nihil, ipsum placeat eos minima corporis saepe sint rem quam nobis totam tenetur assumenda. Necessitatibus!</p>
+            </div>
+
+            <div class="infoChild">
+                <span> Lorem </span>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil nam architecto perspiciatis consequuntur culpa sit repellat? Pariatur rerum voluptatum doloribus a exercitationem? Natus, unde? Ut ipsam magnam vitae libero aperiam!</p>
+          </div>
+
+        </div>
+        <div class="imagecredit">
+            <a style="background-color:black;color:white;text-decoration:none;padding:4px 6px;font-family:-apple-system, BlinkMacSystemFont, &quot;San Francisco&quot;, &quot;Helvetica Neue&quot;, Helvetica, Ubuntu, Roboto, Noto, &quot;Segoe UI&quot;, Arial, sans-serif;font-size:12px;font-weight:bold;line-height:1.2;display:inline-block;border-radius:3px" href="https://unsplash.com/@jaymantri?utm_medium=referral&amp;utm_campaign=photographer-credit&amp;utm_content=creditBadge" target="_blank" rel="noopener noreferrer" title="Download free do whatever you want high-resolution photos from Jay Mantri"><span style="display:inline-block;padding:2px 3px"><svg xmlns="http://www.w3.org/2000/svg" style="height:12px;width:auto;position:relative;vertical-align:middle;top:-2px;fill:white" viewBox="0 0 32 32"><title>unsplash-logo</title><path d="M10 9V0h12v9H10zm12 5h10v18H0V14h10v9h12v-9z"></path></svg></span><span style="display:inline-block;padding:2px 3px">Jay Mantri</span></a>
+        </div>
+    </body>
+
+</html>
+
+    
